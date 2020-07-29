3e8f3e5d1837db7d7a66d42136c717cee4c0c991
artur <artur_seilah@hotmail.com>
2020-07-29 20:58:48 +0100

    Configured NextJS and created the first pages

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/home/artur/Documents/ethereum_solidity_developer/blockchain-based-crowdfunding/pages/index.js?entry";
+
+exports.default = function () {
+    return _react2.default.createElement("h1", {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 5
+        }
+    }, "This is the campaign list page!");
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQU0sQUFDakI7MkJBQ0ksY0FBQTs7c0JBQUE7d0JBQUE7QUFBQTtBQUFBLEtBQUEsRUFESixBQUNJLEFBRVA7QUFKRCIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvaG9tZS9hcnR1ci9Eb2N1bWVudHMvZXRoZXJldW1fc29saWRpdHlfZGV2ZWxvcGVyL2Jsb2NrY2hhaW4tYmFzZWQtY3Jvd2RmdW5kaW5nIn0=
