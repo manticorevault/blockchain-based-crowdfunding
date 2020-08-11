588d61ba6885877a28d625389a0d9a5fe6d405ec
manticorevault <artur_seilah@hotmail.com>
2020-08-11 10:16:32 +0100

    Created and stylized the card with the props from the summary, now displaying on the Single View page

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout"
 import Campaign from "../../ethereum/campaign";
+import { Card } from "semantic-ui-react";
 
 class CampaignShow extends Component {
 
@@ -19,10 +20,33 @@ class CampaignShow extends Component {
          };
     }
 
+    renderCards() {
+
+        const {
+            balance,
+            manager,
+            minimumContribution,
+            requestsCount,
+            approversCount
+        } = this.props;
+
+        const items = [
+            {
+                header: manager,
+                meta: "Manager's Address",
+                description: "Manager created this campaign and they can withdraw money",
+                style: { overflowWrap: "break-word" }
+            }
+        ];
+
+        return <Card.Group items={items} />
+    }
+
     render() {
         return (
             <Layout>
                 <h3> Single Page </h3>
+                {this.renderCards()}
             </Layout>
         )
     }
