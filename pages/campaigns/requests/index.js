07817067c58001b7ba9b7a6870c054871c190365
manticorevault <artur_seilah@hotmail.com>
2020-08-20 15:03:21 +0100

    Fixed the requestCount to return an Int instead of a String

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -11,7 +11,7 @@ class RequestIndex extends Component {
         const requestCount = await campaign.methods.getRequestsCount().call();
 
         const requests = await Promise.all(
-            Array(requestCount).fill().map((element, index) => {
+            Array(parseInt(requestCount)).fill().map((element, index) => {
                 return campaign.methods.requests(index).call(); 
             })
         );
